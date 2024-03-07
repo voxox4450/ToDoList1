bc50a7e9fc9cead48522cd0d88f801d6099b5e94
hubert.krejpcio <hubert.krejpcio@ACARS-0099>
2024-03-07 14:57:56 +0100

    Add project files.

---
+++ ToDoList/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ToDoList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private UserControl1 userControl1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            userControl1 = new UserControl1(this);
+        }
+        public void ButtonExit(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        public void Show()
+        {
+            MainMenu.Visibility = Visibility.Visible;
+        }
+
+        public void Hide()
+        {
+            MainMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+            Frame.Navigate(new UserControl1(this));
+        }
+    }
+}
---
+++ ToDoList/UserControl1.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToDoList
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public MainWindow MainWindow { get; set; }
+
+        public UserControl1(MainWindow mainWindow)
+        {
+            InitializeComponent();
+
+            MainWindow = mainWindow;
+        }
+        public void Show()
+        {
+            AddControl.Visibility = Visibility.Visible;
+        }
+        public void Hide()
+        {
+            AddControl.Visibility = Visibility.Collapsed;
+        }
+
+        private void AddClick(object sender, RoutedEventArgs e)
+        {
+            Hide();
+            MainWindow.Show();
+            string contentText = content.Text;
+            DateTime EndDate = DateTime.Parse(end.Text);
+            DateTime StartDate = DateTime.Parse(start.Text);
+            string Priority = prio.Text;
+            string Status = status.Text;
+
+        }
+        public class Note()
+        {
+
+        }
+    }
+}
